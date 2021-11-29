55b64eaaec0b99c2f0bcafb293093d44e70b2d96
Anđela Ćulibrk <aculibrk@smart.edu.rs>
2021-11-29 10:56:50 +0100

    Init commit

--- public/game.js
+++ public/game.js
@@ -1,2 +1,12 @@
 // KLIJENTSKA JS
 console.log('helouuu');
+//dodavanje muzike
+var btn = document.getElementById("play");
+btn.addEventListener('click',function()
+{
+    var audio = new Audio('audio.mp3');
+    audio.play();
+    console.log("music played")
+});
+
+background
